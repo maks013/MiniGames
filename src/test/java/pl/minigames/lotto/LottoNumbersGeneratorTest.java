10e4c42005cef46a911e55878ef8a989fa12b6cf
Max <maksymilian.ulanecki@gmail.com>
2023-03-29 17:08:54 +0200

    added unit tests for numbers generator class

---
+++ src/test/java/pl/minigames/lotto/LottoNumbersGeneratorTest.java
@@ -0,0 +1,41 @@
+package pl.minigames.lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoNumbersGeneratorTest {
+    @Test
+    public void should_return_six_numbers() {
+        //given
+        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();
+        int size = 6;
+
+        //when
+        lottoNumbersGenerator.generateSixNumbers();
+
+        //then
+        assertEquals(size, lottoNumbersGenerator.getSixWinningNumbers().size());
+    }
+
+    @Test
+    public void testSixWinningNumbers() {
+        //given
+        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();
+        Set<Integer> numbers = new HashSet<>();
+
+        //when
+        for (int i = 1; i < 7; i++) {
+            numbers.add(i);
+        }
+        lottoNumbersGenerator.setSixWinningNumbers(numbers);
+        Set<Integer> winningNumbers = lottoNumbersGenerator.getSixWinningNumbers();
+
+        //then
+        assertEquals(numbers, winningNumbers);
+    }
+
+}
