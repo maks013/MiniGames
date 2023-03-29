6f493bd439b9c5b038905766cc3c5c7fb8500768
Max <maksymilian.ulanecki@gmail.com>
2023-03-29 16:46:56 +0200

    created game messages class

---
+++ src/main/java/pl/minigames/lotto/LottoGameMessages.java
@@ -0,0 +1,8 @@
+package pl.minigames.lotto;
+
+public class LottoGameMessages {
+    public static final String NAME_OF_GAME = "------Lotto Game------";
+    public static final String SEPARATION = "----------------------";
+    public static final String WIN = "Udało się! Gratulacje!";
+    public static final String LOSE = "Niestety nie udało się. Trafionych liczb: ";
+}
