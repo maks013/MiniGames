ef90a4403290ee7b7c37c1994135e902705282c0
Max <maksymilian.ulanecki@gmail.com>
2023-03-29 17:08:23 +0200

    changed generateSixNumbers method using java stream api

--- src/main/java/pl/minigames/lotto/LottoNumbersGenerator.java
+++ src/main/java/pl/minigames/lotto/LottoNumbersGenerator.java
@@ -1,21 +1,25 @@
 package pl.minigames.lotto;
 
+import lombok.Data;
 import lombok.Getter;
 
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-@Getter
+@Data
 public class LottoNumbersGenerator {
     Set<Integer> sixWinningNumbers = new HashSet<>();
 
     public void generateSixNumbers() {
-        for (int i = 0; i < 6; i++) {
-            Random random = new Random();
-            int randomNumber = random.nextInt(98) + 1;
-            sixWinningNumbers.add(randomNumber);
-        }
+        Set<Integer> numbers = new Random()
+                .ints(1, 99)
+                .distinct()
+                .limit(6)
+                .boxed()
+                .collect(Collectors.toSet());
+        sixWinningNumbers.addAll(numbers);
     }
 
 }
