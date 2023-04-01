929c5c0b8990cac2820967497c3fe408b8d041c0
Maksymilian Ulanecki <maks.u@vp.pl>
2023-04-02 00:32:41 +0200

    changed parameters to private final and improved tests

--- src/main/java/pl/minigames/lotto/LottoConfig.java
+++ src/main/java/pl/minigames/lotto/LottoConfig.java
@@ -3,6 +3,5 @@ package pl.minigames.lotto;
 class LottoConfig {
     static final int LOWEST_VALUE_OF_RANGE = 1;
     static final int HIGHEST_VALUE_OF_RANGE = 99;
-    static final int MIN_QUANTITY_IN_SET = 0;
     static final int MAX_QUANTITY_IN_SET = 6;
 }
