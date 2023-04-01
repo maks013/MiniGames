817888c1ac01201f4106d695b3f8dd43537b0933
Maksymilian Ulanecki <maks.u@vp.pl>
2023-04-02 00:24:12 +0200

    added lotto result class

---
+++ src/main/java/pl/minigames/lotto/LottoResult.java
@@ -0,0 +1,30 @@
+package pl.minigames.lotto;
+
+import java.util.Set;
+
+import static pl.minigames.lotto.LottoGameMessages.*;
+
+class LottoResult {
+    private final LottoHitByThePlayer lottoHitByThePlayer = new LottoHitByThePlayer();
+
+    void getResult(Set<Integer> randomNumbers, Set<Integer> inputedByThePlayer) {
+        winOrLose(randomNumbers, inputedByThePlayer);
+        returnNumbers(randomNumbers, inputedByThePlayer);
+    }
+
+    private void winOrLose(Set<Integer> randomNumbers, Set<Integer> inputedByThePlayer) {
+        System.out.println(SEPARATION);
+        if (randomNumbers.equals(inputedByThePlayer)) {
+            System.out.println(WIN);
+        } else {
+            System.out.println(LOSE + lottoHitByThePlayer.calculateHits(randomNumbers, inputedByThePlayer));
+        }
+    }
+
+    private void returnNumbers(Set<Integer> randomNumbers, Set<Integer> inputedByThePlayer) {
+        System.out.println(SEPARATION);
+        System.out.println(YOUR_NUMBERS + inputedByThePlayer);
+        System.out.println(WINNING_NUMBERS + randomNumbers);
+
+    }
+}
